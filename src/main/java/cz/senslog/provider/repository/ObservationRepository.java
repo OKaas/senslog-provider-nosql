c02f73e03a8fc03a0dc6a605e9bf377bf29a706a
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-12-19 12:25:40 +0100

    added pageable observations by sensor id

--- src/main/java/cz/senslog/provider/repository/ObservationRepository.java
+++ src/main/java/cz/senslog/provider/repository/ObservationRepository.java
@@ -2,8 +2,11 @@ package cz.senslog.provider.repository;
 
 import cz.senslog.model.db.Observation;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Created by OK on 02-Nov-18.
@@ -11,6 +14,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "observation", path = "observation")
 public interface ObservationRepository extends MongoRepository<Observation, ObjectId> {
 
+    List<Observation> findAllBySensorId(ObjectId sensorId, Pageable pageable);
     /* --- Collaborates --- */
 }
 
